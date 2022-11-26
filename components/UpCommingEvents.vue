7053b8622bd7ce98173c72ecd41c60eb68d90127
hiren14 <hiren14lalni@gmail.com>
2022-11-26 09:12:14 +0530

    add this to site

--- components/UpCommingEvents.vue
+++ components/UpCommingEvents.vue
@@ -13,7 +13,9 @@
       <div class="cards-grid">
         <div v-for="(event, index) in events" :key="index" class="card">
           <div class="card-side smooth-transition">
+            <a :href="event.url" target="_blank">
             <img :src="event.image" alt="cityImage" />
+            </a>
             <div class="texts">
               <h4 class="eventName" v-html="event.name">{{event.name}}</h4>
               <p class="event">
@@ -21,6 +23,7 @@
                 Time: {{ event.time }} <br />
               </p>
               <a :href="event.url" target="_blank">
+               
                 <p
                   :class="[
                     'venue smooth-transition',
@@ -51,12 +54,12 @@ export default {
     return {
       events: [
         {
-          name: "HTML and CSS Workshop",
+          name: "Dive Into Docker",
           details:
             "We're a group of passionate developers who are here to help you grow your programming skills.<br /><br />Join us for a workshop Glance At Web Designing about HTML and CSS by CodeHives Club.<br />This workshop is on Friday, Sept 30th from 10:30am - 2:00 pm (ET) and will cover the fundamentals of CSS and HTML with an emphasis on how to use each to make your websites more functional.<br />Learn how to create effective layouts, design images as backgrounds, add animation to your content and much more!",
-          image: require("~/assets/event/eve_3.jpg"),
-          venue: "Seminar Hall, Cse Dept, GecPatan",
-          date: "30th Sept. 2022",
+          image: require("~/assets/event/event_4.png"),
+          venue: "Seminar Hall, Civil Dept, GecPatan",
+          date: "28th-29th Nov. 2022",
           time: "10:30 AM to 2:00 PM",
           url: "/registration",
           active: true
